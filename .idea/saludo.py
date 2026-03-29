800f2b7caf2cd549146df42d706c9b560dc746f0
ChrisMar0512 <christian.mar@utec.edu.pe>
2026-03-29 18:29:37 -0500

    agregar funcion actualizar
    se agrega funcion actualizar en branch conflicto

--- .idea/saludo.py
+++ .idea/saludo.py
@@ -1,12 +1,17 @@
 def saludar(nombre):
     return f"Hola, {nombre}! Este es mi primer push al hackaton."
 
-if __name__ == "__main__":
-    print(saludar("Mundo"))
-
 def despedir(nombre):
     return f"Hasta luego, {nombre}! Nos vemos en el hackaton."
+
+def actualizar(nombre):
+    return f"Hola {nombre}, versión desde la rama conflicto!"
 
 if __name__ == "__main__":
     print(saludar("Mundo"))
     print(despedir("Mundo"))
+    print(actualizar("Mundo"))
+
+
+
+
