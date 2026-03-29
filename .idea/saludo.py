51ecd30ab24d9f720225a31977445ca1a2cd4cb6
ChrisMar0512 <christian.mar@utec.edu.pe>
2026-03-29 16:22:46 -0500

    funcion despedir
    se agrego la funcion despedir a saludo.py

--- .idea/saludo.py
+++ .idea/saludo.py
@@ -3,3 +3,10 @@ def saludar(nombre):
 
 if __name__ == "__main__":
     print(saludar("Mundo"))
+
+def despedir(nombre):
+    return f"Hasta luego, {nombre}! Nos vemos en el hackaton."
+
+if __name__ == "__main__":
+    print(saludar("Mundo"))
+    print(despedir("Mundo"))
